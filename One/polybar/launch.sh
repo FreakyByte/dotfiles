e5de227c0dbb070c8ba5f1dc7448b05eb85cc591
FreakyByte <michi.reitmeir@gmail.com>
2021-02-25 13:56:42 +0100

    Added all dotfiles from reitiOne. Created folders for different machines, so far the dotfiles are all the same.

---
+++ One/polybar/launch.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
+
+# Launch bars
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar -c /home/reiti/.config/polybar/config main >>/tmp/polybar1.log 2>&1 & disown
+polybar -c /home/reiti/.config/polybar/config secondary >>/tmp/polybar2.log 2>&1 & disown
+polybar -c /home/reiti/.config/polybar/config tray >>/tmp/polybar2.log 2>&1 & disown
+
+echo "Bars launched..."
