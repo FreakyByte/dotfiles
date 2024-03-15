e6ad642b1927625115e82ae011aeb5ce8eea82ab
FreakyByte <michi.reitmeir@gmail.com>
2024-03-15 23:11:42 +0100

    Switched ls out for eza.

--- fish/config.fish
+++ fish/config.fish
@@ -4,4 +4,7 @@ cat ~/.cache/wal/sequences
 # quickly remove newline characters at end of file
 alias chompeof="perl -pi -e 'chomp if eof'"
 
+# use eza instead of ls for nicer colors
+alias ls="eza"
+
 #starship init fish | source
