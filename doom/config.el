31d9b88ea83e1bd6c8843d03e902386be59f3cd6
FreakyByte <michi.reitmeir@gmail.com>
2024-11-21 10:42:20 +0100

    emacs: model theory snippets

--- doom/config.el
+++ doom/config.el
@@ -952,7 +952,7 @@ INTER signals whether the function has been called interactively."
 (add-hook 'roam-pseudohook 'org-latex-preview-whole-buffer)
 
 (setq org-latex-mathoperators (list
-        "acl" "Ad" "Aut" "bd" "card" "cl" "coker" "Covar" "dcl" "ED" "End" "Ext" "fr" "Frac" "GL" "Hom" "id" "im" "ind" "lexmin" "lexmax" "Li" "Mat" "ord" "RM" "sinc" "SL" "SO" "Spec" "st" "Sub" "Th" "tp" "Tor" "Var"))
+        "acl" "Ad" "Aut" "bd" "card" "cl" "coker" "Covar" "dcl" "ded" "ED" "End" "Ext" "fr" "Frac" "GL" "Hom" "id" "im" "ind" "lexmin" "lexmax" "Li" "Mat" "ord" "RM" "sinc" "SL" "SO" "Spec" "st" "Sub" "Th" "tp" "Tor" "Var"))
 (dolist (macro org-latex-mathoperators)
   (setq org-latex-preview-preamble (concat org-latex-preview-preamble "\\DeclareMathOperator{\\" macro "}{" macro "}"))
   (add-to-list 'org-roam-ui-latex-macros (cons (concat "\\" macro) (concat "\\operatorname{" macro "}")) t)
